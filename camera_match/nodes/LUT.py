80dd2dc5b18a4585fe6b8dd17151ab9948033f6d
losbello <130603279+losbello@users.noreply.github.com>
2023-09-15 13:44:29 +0200

    Update LUT.py
    Added a progress bar

--- camera_match/nodes/LUT.py
+++ camera_match/nodes/LUT.py
@@ -1,8 +1,10 @@
 import numpy as np
+from typing import Tuple, Any
 from scipy.spatial.distance import cdist
 from colour.algebra import table_interpolation_tetrahedral
 from colour import LUT3D, read_LUT
 from .Node import Node
+from tqdm import tqdm
 
 from typing import Optional, Any, Tuple
 from numpy.typing import NDArray
@@ -17,7 +19,6 @@ class RBF(Node):
     def __init__(self, size: int=33, radius: float=5.0, layers: int=10, smoothing: float=0.001):
         self.size = size
         self.LUT = None
-
         self.radius = radius
         self.layers = layers
         self.smoothing = smoothing
@@ -27,17 +28,31 @@ class RBF(Node):
 
         model = xalglib.rbfcreate(3, 3)
         xalglib.rbfsetpoints(model, data.tolist())
-        xalglib.rbfsetalgohierarchical(model, self.radius, self.layers, self.smoothing)
+
+        # Part 1: Setting up the RBF model
+        with tqdm(total=50, desc="Setting up RBF Model") as progress_bar:
+            progress_bar.update(10)  # Update progress by 10% for the setup part
+
+            xalglib.rbfsetalgohierarchical(model, self.radius, self.layers, self.smoothing)
+            progress_bar.update(40)  # Update progress by 40% for hierarchical setup
+
         xalglib.rbfbuildmodel(model)
 
-        grid = np.linspace(0, 1, self.size).tolist()
-        table = xalglib.rbfgridcalc3v(model, grid, self.size, grid, self.size, grid, self.size)
+        # Part 2: Building the LUT table
+        with tqdm(total=50, desc="Building LUT Table") as progress_bar:
+            grid = np.linspace(0, 1, self.size).tolist()
+            table = xalglib.rbfgridcalc3v(model, grid, self.size, grid, self.size, grid, self.size)
 
-        # xalglib outputs coordinates in (z, y, x). Swapping axis 0 and 2
-        # gives (x, y, z) which is needed for the LUT table.
-        LUT_table = np.reshape(table, (self.size, self.size, self.size, 3)).swapaxes(0, 2)
+            # xalglib outputs coordinates in (z, y, x). Swapping axis 0 and 2
+            # gives (x, y, z) which is needed for the LUT table.
+            LUT_table = np.reshape(table, (self.size, self.size, self.size, 3)).swapaxes(0, 2)
 
-        self.LUT = LUT3D(table=LUT_table)
+            self.LUT = LUT3D(table=LUT_table)
+            progress_bar.update(50)  # Update progress by 50% for table building
+
+        # Close the progress bar
+        progress_bar.close()
+
         return (self(source), target)
 
     def __call__(self, RGB: NDArray[Any]) -> NDArray[Any]:
